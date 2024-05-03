6a5d180893c815cad9017256d278729e3a25e5a6
Aurore <159303098+AuroreTnr@users.noreply.github.com>
2024-05-03 15:17:27 +0200

    mon profil github

--- projets/aside/script.js
+++
@@ -1,9 +0,0 @@
-const navBar = document.querySelector('.nav-bar');
-
-const navBtn = document.querySelector('.burger-btn');
-
-navBtn.addEventListener('click', handleClickBurgerBtn)
-function handleClickBurgerBtn(e){
-    navBar.classList.toggle("active")
-    navBtn.classList.toggle("active")
-}
